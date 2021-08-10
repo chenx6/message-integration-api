c6374127e3877b476f40b2daf5aa4ce301ef97ea
chen_null <abc82766@gmail.com>
2021-08-10 22:01:11 +0800

    修复bili API 的 默认 limit 值

--- routers/bili.py
+++ routers/bili.py
@@ -9,6 +9,6 @@ router = APIRouter(prefix="/api", tags=["bili"])
 
 
 @router.get("/bili", response_model=ItemsResp)
-def bilibili_hot_anime(start: int = 0, limit: int = 0, db: Session = Depends(get_db)):
+def bilibili_hot_anime(start: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     items = get_bili_hot_anime(db, start, limit)
     return ItemsResp(status="success", items=items)
